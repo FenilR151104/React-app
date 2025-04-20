ed57e81f87f19edd03f4356f347b69c32cb86261
Fenil R Mahesuriya <fenilmahesuriya@gmail.com>
2025-04-20 13:57:36 +0530

    Alert.js

--- Alert.js
+++ Alert.js
@@ -6,8 +6,15 @@ export default function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <div style={{height: '50px'}}> 
+        {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+        </div>}
         </div>
   )
 }
+{/* this is used to give height to the alert box so that it will not overlap with other components */}
+{/* alert is used to show the message in the alert box and alert-dismissible is used to close the alert box */}
+{/* fade show is used to show the alert box with fade effect */}
+{/* role is used for accessibility purpose */}
+{/* strong is used to make the text bold */}
